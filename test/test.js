2337f77d7ce27f3a9a536520d678b015cf0583f5
DCueto <dcueto14.info@gmail.com>
2018-01-25 19:31:31 +0100

    Initial Commit

---
+++ test/test.js
@@ -0,0 +1,32 @@
+
+const expect = require('chai').expect;
+const platzom = require('..').default;
+
+
+describe('#platzom', function(){
+
+	it('Si la palabra termina en "ar", se le quitan esos dos carácteres.', function(){
+		const translation = platzom('Programar');
+		expect(translation).to.equal('Program');
+	});
+
+	it('Si la palabra inicia con Z se le añade "pe" al final.', function(){
+		const translation = {
+			palabra1: platzom('Zorro'),
+			palabra2: platzom('Zarpar')
+		}
+
+		expect(translation.palabra1).to.equal('Zorrope');
+		expect(translation.palabra2).to.equal('Zarppe');
+	});
+
+	it('Si la palabra traducida tiene 10 o más letras se debe partir a la mitad y unir con un guión del medio.', function(){
+		const translation = platzom('abecedario');
+		expect(translation).to.equal('abece-dario');
+	});
+
+	it('Si la palabra original es un palíndromo, ninguna regla anterior cuenta y se devuelve la misma palabra intercalando mayusculas y minúsculas.', function(){
+		const translation = platzom('sometemos');
+		expect(translation).to.equal('SoMeTeMoS');
+	});
+});
